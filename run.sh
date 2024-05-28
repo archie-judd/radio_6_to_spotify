04e237b0bd3adba44f8767257850cb0eb393a69a
archie-judd <archie.judd@yahoo.com>
2024-05-28 17:12:01 +0100

    rm run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd /Users/Archie/repos/radio_6_to_spotify
-source .envrc
-$(poetry env info --path)/bin/python src/radio_6_to_spotify/handler.py 2>&1 | tee ./radio_6_to_spotify.log
